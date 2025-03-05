a1c8217f3db10f3085a458d02083631e39bb6420
minghao <qinminghao625@163.com>
2025-03-05 14:21:45 +0800

    update

--- videoxl/videoxl/model/language_model/llava_qwen.py
+++ videoxl/videoxl/model/language_model/llava_qwen.py
@@ -2284,7 +2284,7 @@ class LlavaQwenForCausalLM(Qwen2ForCausalLM, LlavaMetaForCausalLM):
                         chunks_total_count = len(record_beacon_activations[high_beacon_ratio][0])
                         all_chunk_idx_list = list(range(chunks_total_count))
                         ground_truth_pos = random.sample(all_chunk_idx_list, count_random)
-                        ground_truth_pos = all_chunk_idx_list[:1] + all_chunk_idx_list[-1:]
+                        # ground_truth_pos = all_chunk_idx_list[:1] + all_chunk_idx_list[-1:]
                         print(f'random choice gt: {ground_truth_pos}')
                         for layer_idx in range(self.memory.config.num_hidden_layers):
                             reload_activations.append( record_beacon_activations[high_beacon_ratio][layer_idx] )
